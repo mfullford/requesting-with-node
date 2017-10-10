161bd2ec691b59345a486c9f753caebe2f2b0017
mfullford <marissafullford@gmail.com>
2017-10-10 16:38:26 -0600

    Changes to app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@
 var movie = require("./movie");
 var threeFavoriteMovies = ["American Beauty", "Baby Mama", "The Royal Tenenbaums"];
 threeFavoriteMovies.forEach(function(film){
-  	//call on your movie modules
   	movie(film);
 });
+
